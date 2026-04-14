443cf21244b1701019dddf6fb523142eb0a94cee
Jan Sistonen <125372178+jansistonen@users.noreply.github.com>
2026-04-14 13:48:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,10 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 #BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+app = Flasj(__name__)
+
 database_url = os.getenv("DATABASE_URL")
+
 print("DATABASE_URL configured:", bool(database_url))
 print("DB target:", database_url.split("@")[-1] if database_url else "missing")
 
